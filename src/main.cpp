7e627e850819f861e7bd0897529dca55591f2ace
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-10-19 00:51:54 -0400

    Remove now unnecessary trim, safeStoi, and safeStod functions since we do not need to store any values except the offense description string

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,7 @@
 #include <algorithm> 
 #include <cctype>
 #include <locale>
-
 using namespace std;
-
-string trim(const string& s) {
-    string result = s;
-    // remove leading whitespace and \r
-    result.erase(result.begin(), find_if(result.begin(), result.end(),
-        [](unsigned char ch){ return !isspace(ch) && ch != '\r'; }));
-    // remove trailing whitespace and \r
-    result.erase(find_if(result.rbegin(), result.rend(),
-        [](unsigned char ch){ return !isspace(ch) && ch != '\r'; }).base(), result.end());
-    return result;
-}
-
-
-
-int safeStoi(const string& s){
-    try{
-        return stoi(s);
-    }catch(...){
-        return 0;
-    }
-}
-
-double safeStod(const string& s){
-    try{
-        return stod(s);
-    }catch(...){
-        return 0.0;
-    }
-}
 
 int main(){
     return 0;
