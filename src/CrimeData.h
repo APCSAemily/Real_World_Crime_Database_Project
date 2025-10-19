4375e146782d69fef2b5406d8533b10df5937739
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-10-19 00:07:36 -0400

    Add comments in CrimeData.h

--- src/CrimeData.h
+++ src/CrimeData.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <string>
 
+// since we only need the offense description to determine how serious a crime is, all the extra information is unnecessary and uses up memory
 struct CrimeData {
     std::string OFFENSE_DESCRIPTION;
-    int severityScore = 0;
+    int severityScore = 0; // this is the value used to choose what crimes to prioritize in the heap implementations.
 };
