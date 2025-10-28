e12ecaae62cc6cbeb76e650094cf65af74dbd495
sofiathefirst2323 <sofiaahumada23@yahoo.com>
2025-10-28 19:17:32 -0400

    Update BinaryHeap.cpp
    heap insertion etc

--- src/BinaryHeap.cpp
+++ src/BinaryHeap.cpp
@@ -7,3 +7,14 @@ int BinaryHeap::right(int i){ return 2*i + 2;}
 
 bool BinaryHeap::isEmpty(){
   return crimeData.empty(); }
+
+void BinaryHeap::insert(const Crime& c){
+  crimeData.push_back(c);
+  goUp(crimeData.size() -1);
+}
+void BinaryHeap::goUp (int i ) {
+  while(i>0 && crimeData[i].HOUR > crimeData[parent(i)].HOUR){
+    swap(crimeData[i], crimeData[parent(i)]);
+    i = parent(i);
+  }
+}
