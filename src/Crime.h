acc2c9208d011ecf5230b5f0d7fe6aab5a4ebc0e
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-10-30 23:17:21 -0400

    Use offense code group instead of offense description in Crime.h since it is more human readable and consistent

--- src/Crime.h
+++ src/Crime.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <string>
 
-// since we only need the offense description to determine how serious a crime is, all the extra information is unnecessary and uses up memory
+// since we only need the offense code group to determine how serious a crime is, all the extra information is unnecessary and uses up memory
 struct Crime {
-    std::string OFFENSE_DESCRIPTION;
+    std::string OFFENSE_CODE_GROUP;
     int severityScore = 0; // this is the value used to choose what crimes to prioritize in the heap implementations.
 };
